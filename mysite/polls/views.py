f75a490b8520f06ae19d5a5fdf56ddaf9405a6d9
Matias <matias_russo@outlook.es>
2024-04-15 11:35:34 -0300

    Optimizo views.py, borrando instancias repetidas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,11 +14,9 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
-
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -46,27 +44,3 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
