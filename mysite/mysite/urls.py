ea7e71531f71ebdf2564065d221a43afa73a7b2f
Matias <matias_russo@outlook.es>
2024-04-15 12:53:22 -0300

    Realizo el cambio de zona horaria que no me dejaba ver las preguntas

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from polls.views import IndexView  # Importa la vista IndexView de la aplicación de encuestas
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),  # Ruta raíz redirigida a index.html
+    path("", IndexView.as_view(), name="index"),  # Usa la vista IndexView para la ruta raíz
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
